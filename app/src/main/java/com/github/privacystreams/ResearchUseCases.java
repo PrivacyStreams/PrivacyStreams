f45849b7be2f85128b8b1445436816522c052b4d
Yuanchun <pkulyc@gmail.com>
2017-02-19 21:23:28 -0500

    add research use cases

---
+++ app/src/main/java/com/github/privacystreams/ResearchUseCases.java
@@ -0,0 +1,192 @@
+package com.github.privacystreams;//package xyz.ylimit.personaldataapp;
+//
+//import xyz.ylimit.personaldataapp.privacystreams.UQI;
+//import xyz.ylimit.personaldataapp.privacystreams.collectors.location.Locations;
+//import xyz.ylimit.personaldataapp.privacystreams.collectors.statistic.Statistics;
+//import xyz.ylimit.personaldataapp.privacystreams.generic.Callback;
+//import xyz.ylimit.personaldataapp.privacystreams.generic.MultiItemStream;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.app.AppEvent;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.audio.Audio;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.call.CallLog;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.environment.Light;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.location.GeoLocation;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.motion.Accelerometer;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.sms.SMS;
+//import xyz.ylimit.personaldataapp.privacystreams.providers.system.DeviceState;
+//import xyz.ylimit.personaldataapp.privacystreams.purposes.Purpose;
+//import xyz.ylimit.personaldataapp.privacystreams.utils.time.Duration;
+//
+//import static java.lang.Thread.sleep;
+//import static xyz.ylimit.personaldataapp.privacystreams.transformations.map.Mappers.setField;
+//
+///**
+// * Some show cases of PrivacyStreams
+// */
+//public class ResearchUseCases {
+//
+//    public ResearchUseCases() {
+//    }
+//
+//    /**
+//     * paper: From Awareness to Repartee: Sharing Location within Social Groups
+//     * in: CHI 2008
+//     * Connecto displays context and location information amongst small groups of friends.
+//     * Specifically, it collects semantic location and the length of time a person has been
+//     * at their asLastKnown location.
+//     */
+//    void Connecto_CHI_08() {
+//        UQI
+//                .getDataItems(GeoLocation.asUpdates(), Purpose.feature("displaying location amongst your friends"))
+//                .map(setField("semantic_loc", Locations.geotag(GeoLocation.COORDINATES)))
+//                .localGroupBy("semantic_loc")
+//                .setGroupField("duration", Statistics.range(GeoLocation.TIMESTAMP))
+//                .list();
+//    }
+//
+//    /**
+//     * paper: Unobtrusive Sleep Monitoring using Smartphones
+//     * in: PervasiveHealth 2013
+//     * BES infers sleep duration using the fusion model of light intensity,
+//     * duration of phone lock, duration of stationary status, duration of silence.
+//     */
+//    void BES_PervasiveHealth_2013() {
+//        Purpose purpose = Purpose.feature("sleep monitoring");
+//        MultiItemStream lightStream = UQI
+//                .getDataItems(Light.asUpdates(Duration.seconds(60)), purpose)
+//                .project("timestamp", Light.ILLUMINANCE);
+//
+//        MultiItemStream phoneLockStream = UQI
+//                .getDataItems(DeviceState.asUpdates(Duration.seconds(60)), purpose)
+//                .project("timestamp", DeviceState.LOCKED);
+//
+//        MultiItemStream stationaryStream = UQI
+//                .getDataItems(Accelerometer.asUpdates(), purpose)
+//                .map(setField("timestamp", round(Motion.TIMESTAMP, Duration.seconds(60))))
+//                .localGroupBy("timestamp")
+//                .setGroupField("isStationary", underThreshold(Motion.values, 0));
+//
+//        MultiItemStream silenceStream = UQI
+//                .getDataItems(Microphone.asUpdates(Duration.seconds(60), Duration.seconds(60)), purpose)
+//                .map(setField("timestamp", round(Audio.TIMESTAMP, Duration.seconds(60))))
+//                .map("loudness", calculateLoudness(Audio.URI))
+//                .map(setField("isSlient", underThreshold(Motion.values, 0));
+//
+//        MultiItemStream fusionStream = UQI
+//                .fusionStream("timestamp", lightStream, phoneLockStream, stationaryStream, silenceStream);
+//    }
+//
+//    /**
+//     * paper: Unobtrusive Sleep Monitoring using Smartphones
+//     * in: SenSys 2012
+//     * IODetector detects whether the environment is indoor or outdoor through
+//     * the fusion model of light intensity, cellular signal strength and magnetic field intensity.
+//     * Specifically, it checks whether the light intensity is greater than a threshold.
+//     */
+//    void IODetector_SenSys_2012() {
+//        Purpose purpose = Purpose.feature("sleep monitoring");
+//        MultiItemStream lightStream = UQI
+//                .getDataItems(Light.asUpdates(Duration.seconds(60)), purpose)
+//                .project("timestamp", Light.ILLUMINANCE);
+//
+//        MultiItemStream cellurSignalStream = UQI
+//                .getDataItems(Cellular.asUpdates(Duration.seconds(60)), purpose)
+//                .project("timestamp", Cellular.SIGNAL_STRENGTH);
+//
+//        MultiItemStream magneticIntensityStream = UQI
+//                .getDataItems(MagnetFieldSensor.asUpdates(), purpose)
+//                .map(setField("timestamp", round(EnvSensor.TIMESTAMP, Duration.seconds(60))))
+//                .localGroupBy("timestamp")
+//                .setGroupField("magnet_field_intensity", average(EnvSensor.values));
+//
+//        MultiItemStream fusionStream = UQI
+//                .fusionStream("timestamp", lightStream, cellurSignalStream, magneticIntensityStream);
+//    }
+//
+//    /**
+//     * paper: Automatically Characterizing Places with Opportunistic CrowdSensing using Smartphones
+//     * in: UbiComp 2012
+//     * CrowdSense@Place categorizes places using opportunstically collected images and
+//     audio clips crowdsourced in smartphone users. Specifically, it only samples images
+//     and audio clips once user starts a foreground activity.
+//     */
+//    void CrowdSense_UbiComp_2012() {
+//        Purpose purpose = Purpose.feature("opportunstically collecting images and audio");
+//
+//        Callback callback = new Callback() {
+//            @Override
+//            public void invoke(Object input) {
+//                sleep(Duration.seconds(10));
+//                UQI.getDataItem(Camera.take(), xxx);
+//                UQI.getDataItem(Audio.record(), xxx);
+//            }
+//        };
+//
+//        UQI
+//                .getDataItems(AppEvent.asUpdates(), purpose)
+//                .filter(AppEvent.TYPE, AppEvent.Type.START)
+//                .forEach(callback);
+//    }
+//
+//    /**
+//     * paper: MoodScope: Building a Mood Sensor in Smartphone Usage Patterns
+//     * in: MobiSys 2013
+//     * MoodScope is a mood inference engine that applies mood models to smartphone user data
+//     including emails, application usage sessions, web browsing asHistory, and unique clustered
+//     location records. Specifically, it gets the social interaction through the number of exchanges,
+//     the duration of phone calls, and number of words of text messages and emails with 10 most
+//     frequently interacted contacts in every three days.
+//     */
+//    void MoodScope_MobiSys_2013() {
+//        Purpose purpose = Purpose.feature("inferring mood");
+//        UQI
+//                .getDataItems(CallLog.asList(), purpose)
+//                .map(setField("timestamp_3days", round(CallLog.TIMESTAMP, Duration.days(3))))
+//                .localGroupBy("timestamp_3days")
+//                .setGroupField("duration", sum(CallLog.DURATION));
+//
+//        UQI
+//                .getDataItems(SMS.asHistory(), purpose)
+//                .groupBy(SMS.PHONE_NUMBER);
+//
+//    }
+//
+//    /**
+//     * paper: When Attention is not Scarce - Detecting Boredom from Mobile Phone Usage
+//     * in: UbiComp 2015
+//     * Specifically, when collecting last communication activity,
+//     * Broapp calculates the time since last incoming phone call, last notification,
+//     * last outgoing phone call and last read/received/sent SMS; while measuring contexts,
+//     * it gets access to status about audio connection, charging, light, proximity
+//     * (in terms of covered by screen or not), asLastKnown ringer mode, and semantic location;
+//     * it measures usage intensity using battery info in terms of battery change during
+//     * a period of time, number of bytes received and sent during a period of time and
+//     * time spent in communication apps; it also measures externally trigger usage data
+//     * by calculating number of notifications, name and the app category of last notification;
+//     * finally it measures most used app name as well as the app category in a given period of time,
+//     */
+//    void Broapp_UbiComp_2015() {
+//
+//    }
+//
+//    /**
+//     * paper: Discovering different kinds of smartphone users through their application usage behaviors
+//     * in: UbiComp 2016
+//     * This paper clusters different user groups based on smartphone usages. Specifically, it collects up to 10
+//     most recent app names every hour.
+//     */
+//    void UserClassification_UbiComp_2016() {
+//
+//    }
+//
+//    /**
+//     * paper: Discovering different kinds of smartphone users through their application usage behaviors
+//     * in: AAAI 2014
+//     * we looked at getting summaries of activity info to assess depression in people.
+//     */
+//    void depression_AAAI_2014() {
+//
+//    }
+//
+//
+//
+//}
---
+++ app/src/main/java/com/github/privacystreams/UseCases.java
@@ -0,0 +1,277 @@
+package com.github.privacystreams;
+
+import android.content.Context;
+import android.location.LocationManager;
+
+import java.util.List;
+import java.util.Map;
+
+import com.github.privacystreams.core.Callback;
+import com.github.privacystreams.core.Item;
+import com.github.privacystreams.core.UQI;
+import com.github.privacystreams.audio.Audio;
+import com.github.privacystreams.core.providers.browser.BrowserHistory;
+import com.github.privacystreams.core.providers.browser.BrowserSearch;
+import com.github.privacystreams.core.providers.call.CallLog;
+import com.github.privacystreams.core.providers.contact.Contact;
+import com.github.privacystreams.core.providers.document.Document;
+import com.github.privacystreams.core.providers.dummy.Dummy;
+import com.github.privacystreams.core.providers.location.GeoLocation;
+import com.github.privacystreams.core.providers.message.Message;
+import com.github.privacystreams.core.providers.mock.MockItem;
+import com.github.privacystreams.core.providers.notification.Notification;
+import com.github.privacystreams.core.providers.photo.Photo;
+import com.github.privacystreams.core.providers.sms.SMS;
+import com.github.privacystreams.core.purposes.Purpose;
+import com.github.privacystreams.core.utilities.arithmetic.Arithmetics;
+import com.github.privacystreams.core.utilities.common.ItemCommons;
+import com.github.privacystreams.core.utilities.comparison.Comparisons;
+import com.github.privacystreams.core.utilities.list.Lists;
+import com.github.privacystreams.core.utilities.location.Locations;
+import com.github.privacystreams.core.utilities.photo.Photos;
+import com.github.privacystreams.core.utilities.statistic.Statistics;
+import com.github.privacystreams.core.utilities.string.Strings;
+import com.github.privacystreams.core.utilities.time.Times;
+import com.github.privacystreams.core.utils.time.Duration;
+import com.github.privacystreams.core.utils.time.TimeUtils;
+
+/**
+ * Some show cases of PrivacyStreams
+ */
+public class UseCases {
+    private UQI uqi;
+    private Context context;
+
+    public UseCases(Context context) {
+        this.context = context;
+        this.uqi = new UQI(context);
+    }
+
+
+    // For testing
+    public void testDummyData() {
+        uqi
+                .getDataItems(MockItem.asRandomUpdates(20, 100, 500), Purpose.test("test"))
+                .limit(100)
+                .timeout(Duration.seconds(100))
+                .map(ItemCommons.setField("time_round", Arithmetics.roundUp(MockItem.TIME_CREATED, Duration.seconds(5))))
+                .localGroupBy("time_round")
+                .debug();
+//                .forEach(Outputs.uploadToDropbox("<dropbox token here>", "dummy"));
+    }
+
+    public void testBrowerSearchUpdates(){
+        uqi
+                .getDataItems(BrowserSearch.asUpdates(),Purpose.ads("browser search"))
+                .debug();
+    }
+    public void testBrowerHistoryUpdates(){
+        uqi
+                .getDataItems(BrowserHistory.asUpdates(),Purpose.ads("browser history"))
+                .debug();
+    }
+    public void testMessageUpdates(){
+        uqi
+                .getDataItems(Message.asUpdates(), Purpose.ads("message"))
+                .debug();
+    }
+    public void testDocumentUpdates(){
+        uqi
+                .getDataItems(Document.asUpdates(),Purpose.ads("document update"))
+                .debug();
+    }
+    public void testImageUpdates(){
+        uqi
+                .getDataItems(Photo.readFromStorage(),Purpose.ads("photo update"))
+                .debug();
+    }
+
+    public void testMotionSensor(){
+
+
+    }
+
+    public void testBrowserSearchUpdates(){
+        uqi.
+                getDataItems(BrowserSearch.asUpdates(),Purpose.feature("browser_search"))
+                .debug();
+    }
+
+
+    public void testBrowserHistoryUpdates(){
+        uqi.
+                getDataItems(BrowserHistory.asUpdates(),Purpose.feature("browser_history"))
+                .debug();
+    }
+
+    public void testNotifications() {
+        uqi
+                .getDataItems(Notification.asUpdates(), Purpose.feature("test"))
+                .print();
+    }
+
+    // get a count of the #contacts in contact list
+    void getContactCount() {
+        uqi
+                .getDataItems(Contact.asList(), Purpose.feature("estimate how popular you are."))
+                .print();
+    }
+
+    // get recent called 10 contacts’ names
+    List<String> getRecentCalledNames(int n) {
+        List<String> recentCalledPhoneNumbers = uqi
+                .getDataItems(CallLog.asList(), Purpose.feature("getDataItems recent called phone numbers"))
+                .sortBy(CallLog.TIMESTAMP)
+                .limit(n)
+                .asList(CallLog.PHONE_NUMBER);
+        List<String> recentCalledNames = uqi
+                .getDataItems(Contact.asList(), Purpose.feature("getDataItems names of recent called phone numbers"))
+                .filter(Lists.intersects(Contact.PHONES, recentCalledPhoneNumbers))
+                .asList(Contact.NAME);
+        return recentCalledNames;
+    }
+
+    // get a count of calls since 31Oct2015
+    int getCallCountSince() {
+        return uqi
+                .getDataItems(CallLog.asList(), Purpose.feature("know how many calls you made"))
+                .filter(Times.since(CallLog.TIMESTAMP, TimeUtils.format("yyyy-MM-dd", "2015-10-31")))
+                .count();
+    }
+
+
+    // get whether at home
+    boolean isAtHome() {
+        return uqi
+                .getDataItem(GeoLocation.asLastKnown(), Purpose.feature("know whether you are at home."))
+                .check(Locations.atHome(GeoLocation.COORDINATES));
+    }
+
+    void callbackWhenReceivesMessage(String appName, Callback<String> messageCallback){
+        uqi
+                .getDataItems(Message.asIncomingMessages(), Purpose.feature(""));
+    }
+
+    // get the intent when enter an area, the callback will be invoked when the use enters or exits an area
+    void callbackWhenEntersArea(double x, double y, double r, Callback<Boolean> enterAreaCallback) {
+        uqi
+                .getDataItems(GeoLocation.asUpdates(LocationManager.GPS_PROVIDER, 10, 10), Purpose.feature("know when you enter an area"))
+                .setField("inArea", Locations.inArea(GeoLocation.COORDINATES, x,y,r))
+                .onChange("inArea", enterAreaCallback);
+    }
+
+    // handle two-factor auth SMS message
+    void getTwoFactorAuthSMS(String serverPhoneNum, Callback<String> messageCallback) {
+        uqi
+                .getDataItems(SMS.asIncomingMessages(), Purpose.feature("Two-factor authentication"))
+                .filter(Comparisons.eq(SMS.PHONE_NUMBER, serverPhoneNum))
+                .filter(Comparisons.eq(SMS.TYPE, SMS.Type.RECEIVED))
+                .ifPresent(SMS.TEXT, messageCallback);
+    }
+
+    // get location and blur 100 meters for advertisement
+    void passLocationToAd() {
+        List<Double> coordinates = uqi
+                .getDataItem(GeoLocation.asLastKnown(), Purpose.ads("targeted advertisement"))
+                .compute(Locations.blur(GeoLocation.COORDINATES, 100));
+    }
+
+    // get postcode of asLastKnown location
+    String getPostcode() {
+        return uqi
+                .getDataItem(GeoLocation.asLastKnown(), Purpose.feature("get postcode for nearby search"))
+                .compute(Locations.postcode(GeoLocation.COORDINATES));
+    }
+
+    // knowing if a person is making more or less calls than normal
+    boolean isMakingMoreCallsThanNormal() {
+        int callCountLastWeek = uqi
+                .getDataItems(CallLog.asList(), Purpose.feature("get how many calls you made recently"))
+                .filter(Times.recent(CallLog.TIMESTAMP, Duration.days(7)))
+                .count();
+        double callFrequencyLastWeek = (double) callCountLastWeek / 7;
+        int callCountLastYear = uqi
+                .getDataItems(CallLog.asList(), Purpose.feature("get how many calls you made normally"))
+                .filter(Times.recent(CallLog.TIMESTAMP, Duration.days(365)))
+                .count();
+        double callFrequencyLastYear = (double) callCountLastYear / 365;
+        return callFrequencyLastWeek > callFrequencyLastYear;
+    }
+
+    // getting all the photo metadata (but not photos)
+    List<Map<String, String>> getAllPhotoMetadata() {
+        return uqi
+                .getDataItems(Photo.readFromStorage(), Purpose.feature("get metadata of the photos in storage"))
+                .setField("metadata", Photos.getMetadata(Photo.URI))
+                .asList("metadata");
+    }
+
+    // getting how loud it is over the past m seconds,  every n minutes
+    void getAverageLoudnessEveryNSeconds(int m, int n, Callback<Double> loudnessCallback) {
+        uqi
+                .getDataItems(Audio.recordPeriodically(Duration.seconds(m), Duration.seconds(n)),
+                        Purpose.feature("how loud it is periodically"))
+                .setField("loudness", Audios.getLoudness(Audio.URI))
+                .forEach("loudness", loudnessCallback);
+    }
+
+    // calculating sentiment across all SMS
+    double getAverageSentimentOfSMS() {
+        return uqi
+                .getDataItems(SMS.asHistory(), Purpose.feature("calculate the sentiment across all SMS messages"))
+                .setField("sentiment", Strings.sentiment(SMS.TEXT))
+                .outputItems(Statistics.average("sentiment"));
+    }
+
+    // figure out place where person spends the most time (ie home)
+    String getPlaceSpentMostTime() {
+        return uqi
+                .getDataItems(GeoLocation.asHistory(), Purpose.feature("get the place you spent the most time"))
+                .setField("geo_tag", Locations.geotag(GeoLocation.COORDINATES))
+                .localGroupBy("geo_tag")
+                .setGroupField("time_spent", Statistics.range(GeoLocation.TIMESTAMP))
+                .sortBy("time_spent")
+                .reverse()
+                .first()
+                .getField("geo_tag");
+    }
+
+
+    // hard, calculate total number of calls and length of calls per person in call log
+    void getTotalNumberOfCallsPerPerson() {
+        // each Map element is like {"phone_number": "xxxxxxx", "num_of_calls": 10, "length_of_calls": 30000}
+        List<Item> totalNumberOfCallsPerPerson = uqi
+                .getDataItems(CallLog.asList(), Purpose.feature("get the tie relationship with people"))
+                .groupBy(CallLog.PHONE_NUMBER)
+                .setGroupField("num_of_calls", Statistics.count())
+                .setGroupField("length_of_calls", Statistics.sum(CallLog.DURATION))
+                .project(CallLog.PHONE_NUMBER, "num_of_calls", "length_of_calls")
+                .asList();
+    }
+
+
+    // hash the names or phone#s in SMS or call logs, so we can get data like above while mitigating privacy concerns
+    List<String> getHashedPhoneNumbersInSMS() {
+        return uqi
+                .getDataItems(SMS.asHistory(), Purpose.feature("get hashed phone numbers."))
+                .setField("hashed_phone_number", Strings.sha1(SMS.PHONE_NUMBER))
+                .asList("hashed_phone_number");
+    }
+
+
+    // only get location data when accelerometer motion is above a threshold
+    // TODO need stream fusion
+    void getLocationWhenMoving() {
+    }
+
+    // hard, when plugged in get data xyz at rate R, but when not plugged in get data xy at rate R'
+    // TODO need stream fusion
+    void upgradeGranularityWhenPlugged() {
+    }
+
+    // hard, build a sleep monitor that combines streams of data from microphone loudness, location, accelerometer, light, etc
+    // TODO need stream fusion
+    void sleepMonitor() {
+    }
+
+}
