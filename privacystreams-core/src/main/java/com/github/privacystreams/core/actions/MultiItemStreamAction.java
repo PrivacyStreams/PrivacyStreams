de914fc41d2260d5663a74b8197c1ef488185499
Yuanchun <pkulyc@gmail.com>
2017-02-23 00:27:44 -0500

    remove getParameter functions in Actions

--- privacystreams-core/src/main/java/com/github/privacystreams/core/actions/MultiItemStreamAction.java
+++ privacystreams-core/src/main/java/com/github/privacystreams/core/actions/MultiItemStreamAction.java
@@ -7,6 +7,7 @@ import com.github.privacystreams.core.Function;
 import com.github.privacystreams.core.Item;
 import com.github.privacystreams.core.MultiItemStream;
 import com.github.privacystreams.core.UQI;
+import com.github.privacystreams.core.utils.Assertions;
 
 /**
  * Created by yuanchun on 28/11/2016.
@@ -18,7 +19,8 @@ public class MultiItemStreamAction<Tout> extends Function<MultiItemStream, Tout>
     private Function<List<Item>, Tout> itemsOutputFunction;
 
     public MultiItemStreamAction(Function<List<Item>, Tout> itemsOutputFunction) {
-        this.itemsOutputFunction = itemsOutputFunction;
+        this.itemsOutputFunction = Assertions.notNull("itemsOutputFunction", itemsOutputFunction);
+        this.addParameters(itemsOutputFunction);
     }
 
     @Override
@@ -26,10 +28,4 @@ public class MultiItemStreamAction<Tout> extends Function<MultiItemStream, Tout>
         return itemsOutputFunction.apply(uqi, input.readAll());
     }
 
-    @Override
-    protected List<Object> getParameters() {
-        List<Object> parameters = new ArrayList<>();
-        parameters.add(this.itemsOutputFunction);
-        return parameters;
-    }
 }
