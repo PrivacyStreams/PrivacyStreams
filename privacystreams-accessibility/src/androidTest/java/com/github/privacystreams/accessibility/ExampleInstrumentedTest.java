f5eff974d5e2e57922c823c15f3ca2b4425f29c3
Yuanchun <pkulyc@gmail.com>
2017-02-26 21:31:04 -0500

    slight change

--- privacystreams-accessibility/src/androidTest/java/com/github/privacystreams/accessibility/ExampleInstrumentedTest.java
+++ privacystreams-accessibility/src/androidTest/java/com/github/privacystreams/accessibility/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.github.privacystreams.privacystreams_accessibility.test", appContext.getPackageName());
+        assertEquals("com.github.privacystreams.accessibility.test", appContext.getPackageName());
     }
 }
