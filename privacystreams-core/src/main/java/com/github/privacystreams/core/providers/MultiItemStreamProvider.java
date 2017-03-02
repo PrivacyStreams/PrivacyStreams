a69d5c1f8c844d8490f43016956e2c8a0c7c9f3b
Yuanchun Li <pkulyc@gmail.com>
2017-03-01 19:04:34 -0500

    Update MultiItemStreamProvider.java

--- privacystreams-core/src/main/java/com/github/privacystreams/core/providers/MultiItemStreamProvider.java
+++ privacystreams-core/src/main/java/com/github/privacystreams/core/providers/MultiItemStreamProvider.java
@@ -1,7 +1,6 @@
 package com.github.privacystreams.core.providers;
 
 import com.github.privacystreams.core.MultiItemStream;
-import com.github.privacystreams.core.UQI;
 
 /**
  * Created by yuanchun on 29/11/2016.
@@ -14,10 +13,5 @@ public abstract class MultiItemStreamProvider extends StreamProvider<MultiItemSt
         this.output = new MultiItemStream(this.getUQI(), this);
         super.init();
     }
-
-
-
-
-    protected abstract void provide();
 
 }
