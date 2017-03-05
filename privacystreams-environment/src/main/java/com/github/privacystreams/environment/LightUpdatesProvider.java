a09fd9c521ddc8ea5640e278de9b9101831a9f51
Fanglin Chen <chenfanglintc@gmail.com>
2017-03-05 17:57:04 -0500

    add light provider skeleton

---
+++ privacystreams-environment/src/main/java/com/github/privacystreams/environment/LightUpdatesProvider.java
@@ -0,0 +1,14 @@
+package com.github.privacystreams.environment;
+
+import com.github.privacystreams.core.providers.MultiItemStreamProvider;
+
+/**
+ * Created by fanglinchen on 3/5/17.
+ */
+
+public class LightUpdatesProvider extends MultiItemStreamProvider {
+    @Override
+    protected void provide() {
+
+    }
+}
