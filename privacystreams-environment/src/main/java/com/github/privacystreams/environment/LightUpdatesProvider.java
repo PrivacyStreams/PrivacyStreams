c7039b1c5f862ee1bc60a7390eebd33c8993ead0
Fanglin Chen <chenfanglintc@gmail.com>
2017-03-06 13:52:08 -0500

    light sensor tested

--- privacystreams-environment/src/main/java/com/github/privacystreams/environment/LightUpdatesProvider.java
+++ privacystreams-environment/src/main/java/com/github/privacystreams/environment/LightUpdatesProvider.java
@@ -4,8 +4,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.util.Log;
 
+import com.github.privacystreams.core.UQI;
 import com.github.privacystreams.core.providers.MultiItemStreamProvider;
 
 import static android.content.Context.SENSOR_SERVICE;
@@ -16,28 +16,34 @@ import static android.content.Context.SENSOR_SERVICE;
 
 public class LightUpdatesProvider extends MultiItemStreamProvider {
 
-//    private Light data;
-//    private long cTime;                                 // The current time
-//    private float cIntensity;                           // The current light intensity
-    private transient MyLightListener lightListener;    // The light listenor
-    private SensorManager sm;
+    private transient MyLightListener lightListener;
+    private transient SensorManager sensorManager;
+
+    @Override
+    protected void onCancelled(UQI uqi) {
+        super.onCancelled(uqi);
+        sensorManager.unregisterListener(lightListener);
+    }
+
     @Override
     protected void provide() {
-    sm = (SensorManager)getContext().getSystemService(SENSOR_SERVICE);
-    lightListener = new MyLightListener();
+        lightListener = new MyLightListener();
     }
 
     private final class MyLightListener implements SensorEventListener{
 
-        private Sensor lightSensor;                             // Set up the sensor and its manager ready for use
-        MyLightListener(){                                  // The constructor for the light listener
-            lightSensor=sm.getDefaultSensor(Sensor.TYPE_LIGHT);
-            sm.registerListener(this,lightSensor,SensorManager.SENSOR_DELAY_NORMAL);//Normal delay
+        private Sensor lightSensor;
+
+
+        MyLightListener(){
+            sensorManager = (SensorManager)getContext().getSystemService(SENSOR_SERVICE);
+            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            sensorManager.registerListener(this,lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
         }
+
         @Override
         public void onSensorChanged(SensorEvent event) {
-         //   Log.e("Light",String.valueOf(event.values[0]));
-            LightUpdatesProvider.this.output(new Light(event.values[0],System.currentTimeMillis()));        //Output a new Light object
+            output(new Light(event.values[0],System.currentTimeMillis()));
         }
 
         @Override
