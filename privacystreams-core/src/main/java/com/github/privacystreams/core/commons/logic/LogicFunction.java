927321ba3a2d35602a99a2385645c8df20e6f617
Yuanchun <pkulyc@gmail.com>
2017-03-07 17:17:51 -0500

    add logic operators

---
+++ privacystreams-core/src/main/java/com/github/privacystreams/core/commons/logic/LogicFunction.java
@@ -0,0 +1,12 @@
+package com.github.privacystreams.core.commons.logic;
+
+import com.github.privacystreams.core.Item;
+import com.github.privacystreams.core.UQI;
+import com.github.privacystreams.core.commons.ItemFunction;
+import com.github.privacystreams.core.utils.Assertions;
+
+/**
+ * Created by yuanchun on 28/12/2016.
+ * A logic function.
+ */
+abstract class LogicFunction extends ItemFunction<Boolean> {}
