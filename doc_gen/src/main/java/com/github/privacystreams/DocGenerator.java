e7956e14c007fa65d3bad3a9234f9472b149ccd4
Yuanchun <pkulyc@gmail.com>
2017-03-10 00:32:04 -0500

    doclet ready to start

--- doc_gen/src/main/java/com/github/privacystreams/DocGenerator.java
+++ doc_gen/src/main/java/com/github/privacystreams/DocGenerator.java
@@ -1,18 +1,20 @@
 package com.github.privacystreams;
 
 import com.sun.javadoc.*;
+import com.sun.tools.doclets.formats.html.HtmlDoclet;
 import com.sun.tools.doclets.standard.Standard;
 
 /**
  * Generate API documentation for PrivacyStreams.
  */
 
-public class DocGenerator extends Standard {
+public class DocGenerator extends HtmlDoclet {
     public static boolean start(RootDoc root) {
         ClassDoc[] classes = root.classes();
         for (int i = 0; i < classes.length; ++i) {
             System.out.println(classes[i]);
         }
+        System.out.println("I'm printing something!");
         return true;
     }
 }
