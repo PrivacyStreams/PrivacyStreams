5a9d76c7c8088ce635029eca2f3762920b2bce5c
Yuanchun <pkulyc@gmail.com>
2017-03-17 17:37:29 -0400

    slight change

--- privacystreams-core/src/main/java/com/github/privacystreams/commons/item/FieldValueGetter.java
+++ privacystreams-core/src/main/java/com/github/privacystreams/commons/item/FieldValueGetter.java
@@ -7,7 +7,6 @@ import com.github.privacystreams.core.UQI;
 import static com.github.privacystreams.utils.Assertions.notNull;
 
 /**
- * Created by yuanchun on 27/12/2016.
  * A function that gets the value of a field
  */
 
