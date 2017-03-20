127bda18d62fd1ecf49d6e15001a1b3b4bd79e86
Yuanchun <pkulyc@gmail.com>
2017-03-19 22:44:05 -0400

    try to fix ConcurrentModification error

--- privacystreams-core/src/main/java/com/github/privacystreams/utils/DropboxUtils.java
+++ privacystreams-core/src/main/java/com/github/privacystreams/utils/DropboxUtils.java
@@ -30,76 +30,94 @@ public class DropboxUtils {
 
     private static long lastSyncTimestamp = 0;
 
-    public static synchronized void addToWaitingList(UQI uqi, String fileName) {
-        SharedPreferences pref = uqi.getContext().getApplicationContext().getSharedPreferences(Consts.LIB_TAG, Context.MODE_PRIVATE);
-        Set<String> waitingList = pref.getStringSet(DROPBOX_WAITING_LIST, new HashSet<String>());
-        waitingList.add(fileName);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.clear();
-        editor.putStringSet(DROPBOX_WAITING_LIST, waitingList);
-        editor.apply();
-        Logging.debug(LOG_TAG + "Added file to waiting list: " + fileName);
+    private static final Object dropboxWaitingListMutex = new Object();
+    private static final Object dropboxUploadMutex = new Object();
+
+    public static void addToWaitingList(UQI uqi, String fileName) {
+        synchronized (dropboxWaitingListMutex) {
+            SharedPreferences pref = uqi.getContext().getApplicationContext().getSharedPreferences(Consts.LIB_TAG, Context.MODE_PRIVATE);
+            Set<String> waitingList = pref.getStringSet(DROPBOX_WAITING_LIST, new HashSet<String>());
+            waitingList.add(fileName);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            editor.putStringSet(DROPBOX_WAITING_LIST, waitingList);
+            editor.apply();
+            Logging.debug(LOG_TAG + "Added file to waiting list: " + fileName);
+        }
     }
 
-    public static synchronized void syncFiles(UQI uqi) {
+    public static void removeFromWaitingList(UQI uqi, Set<String> fileNamesToRemove) {
+        synchronized (dropboxWaitingListMutex) {
+            SharedPreferences pref = uqi.getContext().getApplicationContext().getSharedPreferences(Consts.LIB_TAG, Context.MODE_PRIVATE);
+            Set<String> waitingList = pref.getStringSet(DROPBOX_WAITING_LIST, new HashSet<String>());
+            waitingList.removeAll(fileNamesToRemove);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            editor.putStringSet(DROPBOX_WAITING_LIST, waitingList);
+            editor.apply();
+            Logging.debug(LOG_TAG + "Removed files from waiting list: " + fileNamesToRemove);
+        }
+    }
+
+    private static boolean syncing = false;
+
+    public static void syncFiles(UQI uqi) {
+        if (syncing) return;
+
         long currentTimestamp = System.currentTimeMillis();
         if (currentTimestamp - lastSyncTimestamp < GlobalConfig.DropboxConfig.leastSyncInterval)
             return;
 
-        try {
-            SharedPreferences pref = uqi.getContext().getApplicationContext().getSharedPreferences(Consts.LIB_TAG, Context.MODE_PRIVATE);
-            Set<String> waitingList = pref.getStringSet(DROPBOX_WAITING_LIST, new HashSet<String>());
+        synchronized (dropboxUploadMutex) {
+            syncing = true;
+            try {
+                SharedPreferences pref = uqi.getContext().getApplicationContext().getSharedPreferences(Consts.LIB_TAG, Context.MODE_PRIVATE);
+                Set<String> waitingList = pref.getStringSet(DROPBOX_WAITING_LIST, new HashSet<String>());
 
-            if (waitingList.isEmpty()) return;
-            if (GlobalConfig.DropboxConfig.onlyOverWifi && !ConnectionUtils.isWifiConnected(uqi)) return;
+                if (waitingList.isEmpty()) return;
+                if (GlobalConfig.DropboxConfig.onlyOverWifi && !ConnectionUtils.isWifiConnected(uqi))
+                    return;
 
-            // Create Dropbox client
-            DbxRequestConfig config = new DbxRequestConfig(Consts.LIB_TAG);
-            DbxClientV2 client = new DbxClientV2(config, GlobalConfig.DropboxConfig.accessToken);
+                // Create Dropbox client
+                DbxRequestConfig config = new DbxRequestConfig(Consts.LIB_TAG);
+                DbxClientV2 client = new DbxClientV2(config, GlobalConfig.DropboxConfig.accessToken);
 
-            Set<String> filesToDelete = new HashSet<>();
-            Set<String> filesToRemoveFromWaitingList = new HashSet<>();
+                Set<String> filesToDelete = new HashSet<>();
+                Set<String> filesToRemoveFromWaitingList = new HashSet<>();
+                Set<String> filesToUpload = new HashSet<>(waitingList);
 
-            Logging.debug(LOG_TAG + "Trying to upload: " + waitingList);
+                Logging.debug(LOG_TAG + "Trying to upload: " + filesToUpload);
 
-            for (String fileToUpload : waitingList) {
-                try {
-                    FileInputStream inputStream = uqi.getContext().openFileInput(fileToUpload);
-                    client.files()
-                            .uploadBuilder("/" + fileToUpload)
-                            .withMode(WriteMode.ADD)
-                            .uploadAndFinish(inputStream);
-                    inputStream.close();
-                    filesToDelete.add(fileToUpload);
-                    filesToRemoveFromWaitingList.add(fileToUpload);
+                for (String fileToUpload : filesToUpload) {
+                    try {
+                        FileInputStream inputStream = uqi.getContext().openFileInput(fileToUpload);
+                        client.files()
+                                .uploadBuilder("/" + fileToUpload)
+                                .withMode(WriteMode.ADD)
+                                .uploadAndFinish(inputStream);
+                        inputStream.close();
+                        filesToDelete.add(fileToUpload);
+                        filesToRemoveFromWaitingList.add(fileToUpload);
+                    } catch (FileNotFoundException e) {
+                        filesToRemoveFromWaitingList.add(fileToUpload);
+                    }
                 }
-                catch (FileNotFoundException e) {
-                    filesToRemoveFromWaitingList.add(fileToUpload);
+
+                Logging.debug(LOG_TAG + "Successfully uploaded: " + filesToUpload);
+
+                removeFromWaitingList(uqi, filesToRemoveFromWaitingList);
+
+                for (String fileToDelete : filesToDelete) {
+                    uqi.getContext().deleteFile(fileToDelete);
                 }
+
+                lastSyncTimestamp = System.currentTimeMillis();
+
+            } catch (IOException | DbxException e) {
+                Logging.warn("error uploading files to Dropbox.");
+                e.printStackTrace();
             }
-
-            Logging.debug(LOG_TAG + "Successfully uploaded: " + waitingList);
-
-            if (!filesToRemoveFromWaitingList.isEmpty()) {
-                Set<String> newWaitingList = new HashSet<>(waitingList);
-                newWaitingList.removeAll(filesToRemoveFromWaitingList);
-                SharedPreferences.Editor editor = pref.edit();
-                editor.clear();
-                editor.putStringSet(DROPBOX_WAITING_LIST, newWaitingList);
-                editor.apply();
-            }
-
-            for (String fileToDelete : filesToDelete) {
-                uqi.getContext().deleteFile(fileToDelete);
-            }
-
-            lastSyncTimestamp = System.currentTimeMillis();
-
-            Logging.debug("Upload finished.");
-
-        } catch (IOException | DbxException e) {
-            Logging.warn("error uploading files to Dropbox.");
-            e.printStackTrace();
+            syncing = false;
         }
     }
 
