9028c0732393d4aa46a5018f23e9e4d88879dfeb
Yuanchun Li <pkulyc@gmail.com>
2017-06-26 14:06:29 -0400

    update IOUtils

--- privacystreams-core/src/main/java/com/github/privacystreams/utils/IOUtils.java
+++ privacystreams-core/src/main/java/com/github/privacystreams/utils/IOUtils.java
@@ -56,25 +56,25 @@ public class IOUtils {
             result.put("event_type", AccessibilityEvent.eventTypeToString(event.getEventType()));
             result.put("event_time", event.getEventTime());
             result.put("package_name", event.getPackageName());
-//            result.put("action", event.getAction());
-//            result.put("class_name", event.getClassName());
-//            result.put("text", event.getText());
-//            result.put("content_description", event.getContentDescription());
-//            result.put("item_count", event.getItemCount());
-//            result.put("current_item_index", event.getCurrentItemIndex());
-//            result.put("is_enabled", event.isEnabled());
-//            result.put("is_password", event.isPassword());
-//            result.put("is_checked", event.isChecked());
-//            result.put("is_full_screen", event.isFullScreen());
-//            result.put("is_scrollable", event.isScrollable());
-//            result.put("before_text", event.getBeforeText());
-//            result.put("from_index", event.getFromIndex());
-//            result.put("to_index", event.getToIndex());
-//            result.put("scroll_x", event.getScrollX());
-//            result.put("scroll_y", event.getScrollY());
-//            result.put("max_scroll_x", event.getMaxScrollX());
-//            result.put("max_scroll_y", event.getMaxScrollY());
-//            result.put("removed_count", event.getRemovedCount());
+            result.put("action", event.getAction());
+            result.put("class_name", event.getClassName());
+            result.put("text", event.getText());
+            result.put("content_description", event.getContentDescription());
+            result.put("item_count", event.getItemCount());
+            result.put("current_item_index", event.getCurrentItemIndex());
+            result.put("enabled", event.isEnabled());
+            result.put("is_password", event.isPassword());
+            result.put("checked", event.isChecked());
+            result.put("full_screen", event.isFullScreen());
+            result.put("scrollable", event.isScrollable());
+            result.put("before_text", event.getBeforeText());
+            result.put("from_index", event.getFromIndex());
+            result.put("to_index", event.getToIndex());
+            result.put("scroll_x", event.getScrollX());
+            result.put("scroll_y", event.getScrollY());
+            result.put("max_scroll_x", event.getMaxScrollX());
+            result.put("max_scroll_y", event.getMaxScrollY());
+            result.put("removed_count", event.getRemovedCount());
             AccessibilityNodeInfo sourceNode = event.getSource();
             result.put("source_node", serialize(sourceNode));
             return result;
@@ -82,26 +82,29 @@ public class IOUtils {
         else if (object instanceof AccessibilityNodeInfo) {
             Map<String, Object> result = new HashMap<>();
             AccessibilityNodeInfo nodeInfo = (AccessibilityNodeInfo) object;
-//            result.put("package_name", nodeInfo.getPackageName());
+            result.put("package", nodeInfo.getPackageName());
             result.put("text", nodeInfo.getText());
-            result.put("class_name", nodeInfo.getClassName());
+            result.put("class", nodeInfo.getClassName());
             result.put("child_count", nodeInfo.getChildCount());
             result.put("content_description", nodeInfo.getContentDescription());
-            result.put("is_enabled", nodeInfo.isEnabled());
+            result.put("enabled", nodeInfo.isEnabled());
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                result.put("view_id", nodeInfo.getViewIdResourceName());
-//                result.put("is_editable", nodeInfo.isEditable());
+                result.put("resource_id", nodeInfo.getViewIdResourceName());
+                result.put("editable", nodeInfo.isEditable());
             }
-//            result.put("is_scrollable", nodeInfo.isScrollable());
-//            result.put("is_checked", nodeInfo.isChecked());
-//            result.put("is_password", nodeInfo.isPassword());
-//            result.put("is_checkable", nodeInfo.isCheckable());
-//            result.put("is_clickable", nodeInfo.isClickable());
-//            result.put("is_focused", nodeInfo.isFocused());
-//            result.put("is_focusable", nodeInfo.isFocusable());
-//            result.put("is_visible_to_user", nodeInfo.isVisibleToUser());
-//            result.put("is_long_clickable", nodeInfo.isLongClickable());
-//            result.put("is_selected", nodeInfo.isSelected());
+            result.put("scrollable", nodeInfo.isScrollable());
+            result.put("is_password", nodeInfo.isPassword());
+
+            result.put("checkable", nodeInfo.isCheckable());
+            result.put("checked", nodeInfo.isChecked());
+
+            result.put("clickable", nodeInfo.isClickable());
+
+            result.put("focusable", nodeInfo.isFocusable());
+            result.put("focused", nodeInfo.isFocused());
+            result.put("visible", nodeInfo.isVisibleToUser());
+            result.put("long_clickable", nodeInfo.isLongClickable());
+            result.put("selected", nodeInfo.isSelected());
 
             Rect boundsInScreen = new Rect();
             nodeInfo.getBoundsInScreen(boundsInScreen);
@@ -117,7 +120,7 @@ public class IOUtils {
                     childNodes.add(serialize(childNode));
                 }
             }
-            result.put("child_nodes", childNodes);
+            result.put("children", childNodes);
 
             return result;
         }
