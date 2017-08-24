f591c226601f3c1ceeb2bdfb43ff95a69ebda2e6
chentc <fanglin@cmu.edu>
2017-08-23 22:17:50 -0400

    slight change

--- privacystreams-android-sdk/src/main/java/io/github/privacystreams/communication/WebActivity.java
+++ privacystreams-android-sdk/src/main/java/io/github/privacystreams/communication/WebActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import io.github.privacystreams.core.R;
 import io.github.privacystreams.utils.Logging;
