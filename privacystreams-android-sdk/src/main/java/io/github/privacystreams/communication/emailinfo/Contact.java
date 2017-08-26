d004923028724da132fad575afa62a0880ea5a7d
xiaobing1117 <lianwh14@mails.tsinghua.edu.cn>
2017-08-27 06:48:46 +0800

    delete unused classes and info

--- privacystreams-android-sdk/src/main/java/io/github/privacystreams/communication/emailinfo/Contact.java
+++ privacystreams-android-sdk/src/main/java/io/github/privacystreams/communication/emailinfo/Contact.java
@@ -1,110 +1,16 @@
 package io.github.privacystreams.communication.emailinfo;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.JsonNode;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Generated;
-
+import io.github.privacystreams.core.Item;
 import io.github.privacystreams.core.PStreamProvider;
 import io.github.privacystreams.utils.annotations.PSItemField;
 
+public class Contact extends Item {
 
-
-/**
- * Contact
- * <p>
- *
- *
- */
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
-@Generated("org.jsonschema2pojo")
-@JsonPropertyOrder({
-        "contacts"
-})
-public class Contact extends Sift {
-
-    @JsonProperty("contacts")
-    private List<Person> mContacts = new ArrayList<Person>();
-    @JsonIgnore
-    private Map<String, Object> mAdditionalProperties = new HashMap<String, Object>();
-
-    public Domain getDomain() {
-        return Domain.CONTACT;
-    }
-
-    /**
-     *
-     * @return
-     *     The contacts
-     */
-    @JsonProperty("contacts")
-    public List<Person> getContacts() {
-        return mContacts;
-    }
-
-    /**
-     *
-     * @param contacts
-     *     The contacts
-     */
-    @JsonProperty("contacts")
-    public void setContacts(List<Person> contacts) {
-        this.mContacts = contacts;
-    }
-
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.mAdditionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.mAdditionalProperties.put(name, value);
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder().append(mContacts).append(mAdditionalProperties).toHashCode();
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-        if ((other instanceof Contact) == false) {
-            return false;
-        }
-        Contact rhs = ((Contact) other);
-        return new EqualsBuilder().append(mContacts, rhs.mContacts).append(mAdditionalProperties, rhs.mAdditionalProperties).isEquals();
-    }
-
-    //TODO change these two functions when debug ends
     public static PStreamProvider getContact(String api_key, String api_secret){return new EmailContactProvider(api_key,api_secret);}
-    Contact(){}
 
-    /**fields*/
+    /*Fields*/
 
     @PSItemField(type = String.class)
     public static final String NAME = "name";
